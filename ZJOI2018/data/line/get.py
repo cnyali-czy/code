78d1dd0a38aa583fe45498c8ad9cb9279c8fa8dc
cnyali-czy <1683053325@qq.com>
2018-03-29 21:38:03 +0800

    不需要

--- ZJOI2018/data/line/get.py
+++
@@ -1,8 +0,0 @@
-import os
-import random
-K=[2,3,4,5,5,6,7,8,9,9]
-for i in range(10):
-	ou="line" + str(i + 1) + ".in"
-	ans="line" + str(i + 1) + ".out"
-	os.system("./gen " + str(K[i]) + " " + str(random.randint(1, 100000)) + " > " + ou)
-	os.system("./line < " + ou + " > " + ans)
