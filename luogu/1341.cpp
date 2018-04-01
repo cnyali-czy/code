a57e2df64d79aaea984e3cf6a82e7fec16b711be
cnyali-czy <1683053325@qq.com>
2018-04-01 20:54:30 +0800

    无序字母对

--- luogu/1341.cpp
+++ luogu/1341.cpp
@@ -1,53 +1,57 @@
-#define  REP(i, s, e) for(register int i = s; i <= e ;i++)
 #define DREP(i, s, e) for(register int i = s; i >= e ;i--)
+#define  REP(i, s, e) for(register int i = s; i <= e ;i++)
 
-#define DEBUG fprintf(stderr, "Passing [%s] in LINE %d\n", __FUNCTION__, __LINE__)
-#define chkmin(a, b) a = min(a, b)
+#define DEBUG fprintf(stderr, "Passing [%s] in Line %d\n", __FUNCTION__, __LINE__)
 #define chkmax(a, b) a = max(a, b)
+#define chkmin(a, b) a = min(a, b)
 
-#include <bits/stdc++.h>
+#include <algorithm>
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+#include <cstdio>
+#include <bitset>
+#include <vector>
+#include <cmath>
+#include <queue>
+#include <map>
+#include <set>
 
 using namespace std;
-const int maxn = 3600, maxm = 3600;
-
-int bg[maxn], ne[maxm << 1], to[maxm << 1], e = 1;
-inline void add(int x, int y)
-{
-	e++;
-	to[e] = y;
-	ne[e] = bg[x];
-	bg[x] = e;
-}
+const int maxn = 3000;
 
 int m, n, k;
 
-pair<int, int> a[maxn];
+pair <int, int> a[maxn];
 
-inline int get()
+inline int to(char c)
 {
-	register char c = getchar();
-	while (!(('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z'))) c = getchar();
-	return (int)c;
+	if ('a' <= c && c <= 'z') return c - 'a' + 27;
+	else return c - 'A' + 1;
 }
 
-int degree[300];
-bool vis[300];
-
-void dfs(int x)
+inline char backto(int x)
 {
-	cout << (char)x;
-	vis[x] = 1;
-	vector <int> V;
-	for (register int i = bg[x]; i ; i = ne[i])
-		if (!vis[to[i]]) V.push_back(to[i]);
-	sort(V.begin(), V.end());
-	int siz = V.size();
-	for (int i = 0; i < siz;++i)
-		if (!vis[V[i]])
+	if (1 <= x && x <= 26) return x - 1 + 'A';
+	else return x - 27 + 'a';
+}
+
+const int maxnode = 100;
+
+int degree[maxnode];
+bool G[maxnode][maxnode];
+
+deque <int> ans;
+
+void dfs(int now)
+{
+	REP(i, 1, 70)
+		if (G[i][now])
 		{
-			vis[V[i]] = 1;
-			dfs(V[i]);
+			G[i][now] = G[now][i] = 0;
+			dfs(i);
 		}
+	ans.push_front(now);
 }
 
 int main()
@@ -58,53 +62,43 @@ int main()
 #endif
 	cin >> n;
 	REP(i, 1, n)
-		a[i].first = get(), a[i].second = get();
-#ifdef CraZYali
-	REP(i, 1, n)
-		fprintf(stderr, "%c %c\n", a[i].first, a[i].second);
-#endif
-	REP(i, 1, n)
 	{
-		add(a[i].first, a[i].second);
-		add(a[i].second, a[i].first);
+		char s[5];
+		scanf("%s", s + 1);
+		a[i].first = to(s[1]);
+		a[i].second = to(s[2]);
+		G[a[i].first][a[i].second] = 1;
+		G[a[i].second][a[i].first] = 1;
 		degree[a[i].first]++;
 		degree[a[i].second]++;
 	}
-	int cnt = 0;
-	REP(i, 1, n)
-	{
-		register char c;
-		
-		c = a[i].first;
-		if (!vis[c])
-		{
-			vis[c] = 1;
-			if (degree[c] & 1) cnt++;
-		}
-		
-		c = a[i].second;
-		if (!vis[c])
-		{
-			vis[c] = 1;
-			if (degree[c] & 1) cnt++;
-		}
-	}
-	if (cnt > 2)
+	int pos = 0, cnt = 0;
+	REP(i, 1, 70)
+		cnt += degree[i] % 2 == 1;
+	if (cnt && cnt ^ 2)
 	{
 		cout << "No Solution";
 		return 0;
 	}
-	else
+	if (cnt)
+		REP(i, 1, 70)
+			if (degree[i] & 1)
+			{
+				pos = i;
+				break;
+			}
+	if (!cnt)
+		REP(i, 1, 70)
+			if (degree[i])
+			{
+				pos = i;
+				break;
+			}
+	dfs(pos);
+	while (!ans.empty())
 	{
-		memset(vis, 0, sizeof(vis));
-		int Min = 1000;
-		REP(i, 1, n)
-			chkmin(Min, (int)min(a[i].first, a[i].second));
-		REP(i, 1, n)
-			if (a[i].first == Min)
-				dfs(a[i].first);
-			else if (a[i].second == Min)
-				dfs(a[i].second);
+		putchar(backto(ans.front()));
+		ans.pop_front();
 	}
 	return 0;
 }
