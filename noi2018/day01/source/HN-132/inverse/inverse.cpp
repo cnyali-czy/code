df69468acc99954e723224e4c5e29c3c53475c79
cnyali-czy <1683053325@qq.com>
2018-07-22 20:28:49 +0800

    Useless

--- noi2018/day01/source/HN-132/inverse/inverse.cpp
+++ noi2018/day01/source/HN-132/inverse/inverse.cpp
@@ -4,7 +4,9 @@
 #define chkmin(a, b) a = min(a, b)
 #define chkmax(a, b) a = max(a, b)
 
-#include <bits/stdc++.h>
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
 
 using namespace std;
 const int MOD = 998244353, maxn = 266666 + 10;
@@ -17,35 +19,38 @@ int main()
 	freopen("inverse.in", "r", stdin);
 	freopen("inverse.out", "w", stdout);
 	c[1] = 1;
-	REP(i, 2, maxn - 10) c[i] = (MOD + ((long long)c[i - 1] * (4ll * (long long)i - 2) / ((long long)i + 1))) % MOD;
+	int pren = 1;
 	int T;
 	scanf("%d", &T);
 	while (T --> 0)
 	{
 		scanf("%d", &n);
-
-		bool flag = 1;
-		REP(i, 1, n) scanf("%d", p + i);
-		REP(i, 1, n) if (p[i] ^ i) {flag = 0;break;}
-		if (flag)
+		if (n <= 10)
 		{
+			REP(i, 1, n) scanf("%d", p + i);
+			int ans = 0;
+			while (next_permutation(p + 1, p + 1 + n))
+			{
+				int need = 0;
+				REP(i, 1, n) need += abs(i - p[i]);
+				int cnt = 0;
+				REP(i, 1, n)
+					REP(j, i + 1, n)
+						if (p[i] > p[j]) cnt++;
+				if (cnt == need / 2) ans++;
+				if (ans >= MOD) ans %= MOD;
+			}
+			printf("%d\n", ans);
+		}
+		else
+		{
+			REP(i, 1, n) scanf("%*d");
+			REP(i, pren + 1, n) c[i] = (MOD + (c[i - 1] * (4ll * i - 2) / (i + 1))) % MOD;
+			chkmax(pren, n);
 			printf("%d\n", c[n] - 1);
 			continue;
 		}
 		
-		int ans = 0;
-		while (next_permutation(p + 1, p + 1 + n))
-		{
-			int need = 0;
-			REP(i, 1, n) need += abs(i - p[i]);
-			int cnt = 0;
-			REP(i, 1, n)
-				REP(j, i + 1, n)
-					if (p[i] > p[j]) cnt++;
-			if (cnt == need / 2) ans++;
-			if (ans >= MOD) ans %= MOD;
-		}
-		printf("%d\n", ans);
 	}
 
 	return 0;
