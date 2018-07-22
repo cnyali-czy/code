cb260544ff7c77c6c74d5d1aa5a7d4b994233c19
cnyali-czy <1683053325@qq.com>
2018-07-22 22:08:41 +0800

    fhqTreap

--- uva/11922.cpp
+++ uva/11922.cpp
@@ -18,32 +18,111 @@
 #include <set>
 
 using namespace std;
-const int maxn = 100010;
+const int maxn = 100000 + 10;
 
-template <typename T> T read()
+struct node {int s, v, order, l, r, tag;} t[maxn];
+int t_cnt, root;
+
+inline int newnode(int val)
 {
-	T ans(0), p(1);
-	char c = getchar();
-	while (!isdigit(c)) {
-		if (c == '-') p = -1;
-		c = getchar();
-	}
-	while (isdigit(c)) {
-		ans = ans * 10 + c - 48;
-		c = getchar();
-	}
-	return ans * p;
+	t[++t_cnt] = (node) {1, val, rand(), 0, 0, 0};
+	return t_cnt;
 }
+
+inline void maintain(int x) {t[x].s = t[t[x].l].s + t[t[x].r].s + 1;}
+
+inline void pushdown(int x)
+{
+	int &l(t[x].l), &r(t[x].r);
+
+	if (l) t[l].tag ^= 1;
+	if (r) t[r].tag ^= 1;
+
+	t[x].tag = 0;
+	swap(l, r);
+}
+
+inline void merge(int &x, int a, int b)
+{
+	if (!(a * b)) x = a + b;
+	else
+	{
+		if (t[a].order >= t[b].order)
+		{
+			if (t[a].tag) pushdown(a);
+			merge(t[x = a].r, t[a].r, b);
+		}
+		else
+		{
+			if (t[b].tag) pushdown(b);
+			merge(t[x = b].l, a, t[b].l); 
+		}
+	}
+	maintain(x);
+}
+
+inline void split(int x, int &a, int &b, int siz)
+{
+	if (!x) a = b = 0;
+	else
+	{
+		if (t[x].tag) pushdown(x);
+		int T = t[t[x].l].s + 1;
+		if (T <= siz) split(t[a = x].r, t[x].r, b, siz - T);
+		else split(t[b = x].l, a, t[x].l, siz);
+		maintain(x);
+	}
+}
+
+inline void insert(int val) {merge(root, root, newnode(val));}
+
+inline void reverse(int x, int y)
+{
+
+	int a(0), b(0), c(0);
+	split(root, a, b, x - 1);
+	split(b, b, c, y - x + 1);
+	t[b].tag ^= 1;
+	merge(a, a, c);
+	merge(root, a, b);
+
+/*
+	int l(0), r(0), T(0);
+	split(root, l, T, x - 1);
+	split(T, T, r, y - x + 1);
+	t[T].tag ^= 1;
+	merge(l, l, T);
+	merge(root, l, r);
+*/
+}
+
+inline void print(int x)
+{
+	if (!x) return;
+	if (t[x].tag) pushdown(x);
+	print(t[x].l);
+	printf("%d\n", t[x].v);
+	print(t[x].r);
+}
+
 int m, n, k;
 
 int main()
 {
 #ifdef CraZYali
-	freopen("in", "r", stdin);
-	freopen("out", "w", stdout);
+	freopen("11922.in", "r", stdin);
+	freopen("11922.out", "w", stdout);
 #endif
-
+	cin >> n >> m;
+	srand(n ^ m);
+	REP(i, 1, n) insert(i);
+	while (m --> 0)
+	{
+		register int l, r;
+		scanf("%d%d", &l, &r);
+		reverse(l, r);
+	}
+	print(root);
 
 	return 0;
 }
-
