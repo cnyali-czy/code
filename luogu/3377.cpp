a609f6ff2a94305b95c789cd6c6aa46ed9b77037
cnyali-czy <1683053325@qq.com>
2018-08-13 16:40:49 +0800

    ACCCCCCCCCCCC！删除顺序太坑了

--- luogu/3377.cpp
+++ luogu/3377.cpp
@@ -1,55 +1,74 @@
-#define DREP(i, s, e) for(register int i = s; i >= e ;i--)
 #define  REP(i, s, e) for(register int i = s; i <= e ;i++)
+#define DREP(i, s, e) for(register int i = s; i >= e ;i--)
 
-#define DEBUG fprintf(stderr, "Passing [%s] in Line %d\n", __FUNCTION__, __LINE__)
-#define chkmax(a, b) a = max(a, b)
+#define DEBUG fprintf(stderr, "Passing [%s] in LINE %d\n", __FUNCTION__, __LINE__)
 #define chkmin(a, b) a = min(a, b)
+#define chkmax(a, b) a = max(a, b)
 
-#include <iostream>
-#include <cstdio>
+#include <bits/stdc++.h>
 
 using namespace std;
-const int maxn = 100000 + 10, maxm = 100000 + 10;
+const int maxn = 100000 + 10;
+
+template <typename T> T read()
+{
+	T ans(0), p(1);
+	char c = getchar();
+	while (!isdigit(c))
+	{
+		if (c == '-') p = -1;
+		c = getchar();
+	}
+	while (isdigit(c))
+	{
+		ans = ans * 10 + c - 48;
+		c = getchar();
+	}
+	return ans * p;
+}
 
 int m, n, k;
 
 int fa[maxn];
 int find(int x) {return fa[x] == x ? fa[x] : fa[x] = find(fa[x]);}
 
+bool del[maxn];
+
 struct node
 {
 	node *l, *r;
-	int val, dis;
+	int val, id, dis;
 	node() {dis = 0;l = r = NULL;}
 };
 
-node *root[maxn];
+int dis(node *x) {return x != NULL ? x -> dis : -1;}
 
-void init(int val, int id)
+struct Heap
 {
-	root[id] = new node;
-	root[id] -> val = val;
-}
-
-inline int dis(node *x) {return x != NULL ? x -> dis : -1;}
-
-node *merge(node *A, node *B)
-{
-	if (A == NULL) return B;
-	else if (B == NULL) return A;
-	else
+	node *root;
+	void init(int val, int id)
 	{
-		if (B -> val > A -> val) swap(A, B);
-		A -> r = merge(A -> r, B);
-		if (dis(A -> l) < dis(A -> r)) swap(A -> l, A -> r);
-		A -> dis = dis(A -> r) + 1;
-		return A;
+		root = new node;
+		root -> val = val;
+		root -> id = id;
 	}
-}
-
-int top(int x) {return root[x] -> val;}
-
-void pop(int x) {root[x] = merge(root[x] -> l, root[x] -> r);}
+	pair <int, int> top() {return make_pair(root -> val, root -> id);}
+	node * merge(node * A, node * B)
+	{
+		if (A == NULL) return B;
+		else if (B == NULL) return A;
+		else
+		{
+			if (A -> val > B -> val || A -> val == B -> val && A -> id > B -> id) swap(A, B);
+			A -> r = merge(A -> r, B);
+			if (dis(A -> l) < dis(A -> r)) swap(A -> l, A -> r);
+			A -> dis = dis(A -> r) + 1;
+			return A;
+		}
+	}
+	void merge(Heap B) {root = merge(root, B.root);}
+	void pop() {root = merge(root -> l, root -> r);}
+}H[maxn];
 
 int main()
 {
@@ -60,27 +79,29 @@ int main()
 	cin >> n >> m;
 	REP(i, 1, n)
 	{
-		scanf("%d", &k);
-		init(-k, fa[i] = i);
+		fa[i] = i;
+		H[i].init(read<int>(), i);
 	}
 	while (m --> 0)
 	{
-		register int opt, x, y;
-		scanf("%d%d", &opt, &x);
+		register int opt(read<int>()), x(read<int>()), y;
 		if (opt == 1)
 		{
-			scanf("%d", &y);
-			x = find(x) ; y = find(y);
-			root[y] = merge(root[y], root[x]);
-			fa[x] = y;
-		}
-		else if (opt == 2)
-			if (root[find(x)] != NULL)
+			y = read<int>();
+			if (!del[x] && !del[y] && find(x) ^ find(y))
 			{
-				printf("%d\n", -top(find(x)));
-				pop(find(x));
+				H[find(x)].merge(H[find(y)]);
+				fa[find(y)] = find(x);
 			}
-			else printf("-1\n");
+		}
+		else if (!del[x])
+		{
+			y = find(x);
+			printf("%d\n", H[y].top().first);
+			del[H[y].top().second] = 1;
+			H[y].pop();
+		}
+		else printf("-1\n");
 	}
 	return 0;
 }
