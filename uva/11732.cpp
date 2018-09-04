2fd95f9bff34d953ce45d72b89b4e8c68c47ad98
cnyali-czy <1683053325@qq.com>
2018-09-04 23:33:04 +0800

    搞一个Trie，然后统计前缀，用一个简单一点的转移就行

--- uva/11732.cpp
+++ uva/11732.cpp
@@ -1,108 +1,83 @@
-#define DREP(i, s, e) for(register int i = s; i >= e ;i--)
-#define  REP(i, s, e) for(register int i = s; i <= e ;i++)
-
-#define DEBUG fprintf(stderr, "Passing [%s] in Line %d\n", __FUNCTION__, __LINE__)
-#define chkmax(a, b) a = max(a, b)
-#define chkmin(a, b) a = min(a, b)
-
-#include <bits/stdc++.h>
-
+#include<cstring>
+#include<vector>
 using namespace std;
-const int maxlen = 1000 + 5;
 
-int m, n, k;
+const int maxnode = 4000 * 1000 + 10;
+const int sigma_size = 26;
 
-int idofc(char c)
-{
-	if (isdigit(c)) return c - '0';
-	else if ('A' <= c && c <= 'Z') return c - 'A' + 10;
-	else return c - 'a' + 1 + idofc('Z');
-}
+// 字母表为全体小写字母的Trie
+struct Trie {
+  int head[maxnode]; // head[i]为第i个结点的左儿子编号
+  int next[maxnode]; // next[i]为第i个结点的右兄弟编号
+  char ch[maxnode];  // ch[i]为第i个结点上的字符
+  int tot[maxnode];  // tot[i]为第i个结点为根的子树包含的叶结点总数
+  int sz; // 结点总数
+  long long ans; // 答案
+  void clear() { sz = 1; tot[0] = head[0] = next[0] = 0; } // 初始时只有一个根结点
 
-const int Max = 62;
+  // 插入字符串s（包括最后的'\0'），沿途更新tot
+  void insert(const char *s) {
+    int u = 0, v, n = strlen(s);
+    tot[0]++;
+    for(int i = 0; i <= n; i++) {
+      // 找字符a[i]
+      bool found = false;
+      for(v = head[u]; v != 0; v = next[v])
+        if(ch[v] == s[i]) { // 找到了
+          found = true;
+          break;
+        }
+      if(!found) {
+        v = sz++; // 新建结点
+        tot[v] = 0;
+        ch[v] = s[i];
+        next[v] = head[u];
+        head[u] = v; // 插入到链表的首部
+        head[v] = 0;
+      }
+      u = v;
+      tot[u]++;
+    }
+  }
 
-struct node
-{
-	node *ch[Max];
-	bool is;
-	int siz;
-	node() {siz = is = 0;REP(i, 0, 61) ch[i] = NULL;}
+  // 统计LCP=u的所有单词两两的比较次数之和
+  void dfs(int depth, int u) {
+    if(head[u] == 0) // 叶结点
+      ans += tot[u] * (tot[u] - 1) * depth;
+    else {
+      int sum = 0;
+      for(int v = head[u]; v != 0; v = next[v])
+        sum += tot[v] * (tot[u] - tot[v]); // 子树v中选一个串，其他子树中再选一个
+      ans += sum / 2 * (2 * depth + 1); // 除以2是每种选法统计了两次
+      for(int v = head[u]; v != 0; v = next[v])
+        dfs(depth+1, v);
+    }
+  }
+
+  // 统计
+  long long count() {
+    ans = 0;
+    dfs(0, 0);
+    return ans;
+  }
 };
 
-struct Trie
-{
-	node *root;
-	void dfs_del(node *now)
-	{
-		if (now == NULL) return;
-		REP(i, 0, 61) dfs_del(now -> ch[i]);
-		REP(i, 0, 61) now -> ch[i] = NULL;
-		delete(now);
-	}
-	void clear()
-	{
-		dfs_del(root);
-		root = new node;
-	}
+#include<cstdio>
+const int maxl = 1000 + 10;   // 每个单词最大长度
 
-	void insert(char s[])
-	{
-		register int len = strlen(s + 1);
-		root -> siz++;
-		node *now = root;
-		REP(i, 1, len)
-		{
-			if (now -> ch[idofc(s[i])] == NULL) now -> ch[idofc(s[i])] = new node;
-			now = now -> ch[idofc(s[i])];
-			now -> siz++;
-		}
-		now -> is = 1;
-	}
-	
-	long long ans;
+int n;
+char word[maxl];
+Trie trie;
 
-	void dfs(node *now, int depth)
-	{
-		bool flag = 1;
-		REP(i, 0, 61) if (now -> ch[i] != NULL) {flag = 0;break;}
-		if (flag) ans += (long long)now -> siz * (now -> siz - 1) * depth;
-		else
-		{
-			register long long ret = 0;
-			REP(i, 0, 61)
-				if (now -> ch[i] != NULL) ret += (long long)now -> ch[i] -> siz * (now -> siz - now -> ch[i] -> siz);
-			ans += (long long)ret / 2 * (2 * depth + 1);
-			REP(i, 0, 61)
-				if (now -> ch[i] != NULL) dfs(now -> ch[i], depth + 1);
-		}
-	}
-
-	long long calc()
-	{
-		ans = 0;
-		dfs(root, 0);
-		return ans;
-	}
-}T;
-
-char s[maxlen];
-
-int main()
-{
-#ifdef CraZYali
-	freopen("11732.in", "r", stdin);
-	freopen("11732.out", "w", stdout);
-#endif
-	register int Case(0);
-	while (scanf("%d\n", &n) * n)
-	{
-		T.clear();
-		while (n--)
-		{
-			scanf("%s", s + 1);
-			T.insert(s);
-		}
-		printf("Case %d: %lld\n", ++Case, T.calc());
-	}
-	return 0;
+int main() {
+  int kase = 1;
+  while(scanf("%d", &n) == 1 && n) {
+    trie.clear();
+    for(int i = 0; i < n; i++) {
+      scanf("%s", word);
+      trie.insert(word);
+    }
+    printf("Case %d: %lld\n", kase++, trie.count());
+  }
+  return 0;
 }
