f6030db68b45d3e905ff1481104037eb2f740a57
cnyali-czy <1683053325@qq.com>
2018-09-10 20:12:51 +0800

    　

--- others/noi.ac/2018-09-08/sort.cpp
+++ others/noi.ac/2018-09-08/sort.cpp
@@ -28,6 +28,55 @@ namespace cheat
 	}
 };
 
+inline void rvs(int l, int r)
+{
+	if (l < r)
+	{
+		printf("%d %d\n", l, r);
+		reverse(a + l, a + r + 1);
+	}
+}
+
+int ret;
+
+void work(int l, int r)
+{
+	if (l >= r) return;
+	register int mid = l + r >> 1;
+	work(l, mid);
+	work(mid + 1, r);
+	register int x(mid), y(mid + 1);
+	while (x >= l && a[x] > ret) x--;
+	while (y <= r && a[y] <= ret) y++;
+	x++;y--;
+	rvs(x, y);
+}
+
+void solve(int l, int r)
+{
+	if (l >= r) return;
+	bool flag = 1;
+	REP(i, l + 1, r)
+		if (a[i - 1] > a[i])
+		{
+			flag = 0;
+			break;
+		}
+	if (flag) return;
+	else
+	{
+		ret = a[rand() % (r - l + 1) + l];
+		work(l, r);
+		DREP(i, r, l)
+			if (a[i] <= ret)
+			{
+				solve(l, i);
+				solve(i + 1, r);
+				return;
+			}
+	}
+}
+
 int main()
 {
 #ifdef CraZYali
@@ -35,10 +84,12 @@ int main()
 	freopen("sort.out", "w", stdout);
 #endif
 	cin >> n;
+	srand(n);
 	REP(i, 1, n) scanf("%d", a + i);
 	
-	cheat::work();
-
+//	cheat::work();
+	solve(1, n);
+	printf("-1 -1");
 
 	return 0;
 }
