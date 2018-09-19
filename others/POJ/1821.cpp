f40c8767672f7141eb6a712e3177bb015513904e
cnyali-czy <1683053325@qq.com>
2018-09-19 22:46:04 +0800

    吼啊

--- others/POJ/1821.cpp
+++ others/POJ/1821.cpp
@@ -1,100 +1,84 @@
-#include<stdio.h>
-#include<algorithm>
-#include<queue>
-#include<stack>
-#include<map>
-#include<set>
-#include<vector>
-#include<iostream>
-#include<string.h>
-#include<string>
-#include<math.h>
-#include<stdlib.h>
-#define inff 0x3fffffff
-#define eps 1e-8
-#define nn 21000
-#define mod 1000000007
-typedef long long LL;
-const LL inf64=LL(inff)*inff;
+#define DREP(i, s, e) for(register int i = s; i >= e ;i--)
+#define  REP(i, s, e) for(register int i = s; i <= e ;i++)
+
+#define DEBUG fprintf(stderr, "Passing [%s] in Line %d\n", __FUNCTION__, __LINE__)
+#define chkmax(a, b) a = max(a, b)
+#define chkmin(a, b) a = min(a, b)
+
+#include <algorithm>
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+
 using namespace std;
-int n,k;
-int l[nn],p[nn],s[nn];
-int dp[110][nn];
-int que[nn];
-int l1,r1;
-void add(int id,int i)
+const int maxn = 100 + 5, maxm = 16000 + 10;
+
+template <typename T> inline T read()
 {
-    while(r1>l1)
-    {
-        if(dp[i-1][que[r1-1]]-p[i]*que[r1-1]<=dp[i-1][id]-p[i]*id)
-        {
-            r1--;
-        }
-        else
-        {
-            break;
-        }
-    }
-    que[r1++]=id;
-}
-void dele(int id)
-{
-    while(l1<r1)
-    {
-        if(que[l1]<id)
-        {
-            l1++;
-        }
-        else
-            break;
-    }
-}
-int main()
-{
-    int i,j;
-    while(scanf("%d%d",&n,&k)!=EOF)
-    {
-        for(i=1;i<=k;i++)
-        {
-            scanf("%d%d%d",&l[i],&p[i],&s[i]);
-        }
-        for(i=1;i<=k;i++)
-        {
-            for(j=i+1;j<=k;j++)
-            {
-                if(s[j]<s[i])
-                {
-                    swap(l[i],l[j]);
-                    swap(s[i],s[j]);
-                    swap(p[i],p[j]);
-                }
-            }
-        }
-        for(i=0;i<=n;i++)
-            dp[0][i]=0;
-        int ix;
-        for(i=1;i<=k;i++)
-        {
-            l1=r1=0;
-            add(0,i);
-            for(j=1;j<=n;j++)
-            {
-                dp[i][j]=max(dp[i][j-1],dp[i-1][j]);
-                if(j>=s[i])
-                {
-                    ix=max(0,j-l[i]);
-                    if(ix<s[i])
-                    {
-                        dele(ix);
-                        dp[i][j]=max(dp[i][j],dp[i-1][que[l1]]-p[i]*que[l1]+p[i]*j);
-                    }
-                }
-                if(j<s[i])
-                    add(j,i);
-            }
-        }
-        printf("%d\n",dp[k][n]);
-    }
-    return 0;
+	T ans(0), p(1);
+	char c = getchar();
+	while (!isdigit(c))
+	{
+		if (c == '-') p = -1;
+		c = getchar();
+	}
+	while (isdigit(c))
+	{
+		ans = ans * 10 + c - 48;
+		c = getchar();
+	}
+	return ans * p;
 }
 
+template <typename T> void write(T x)
+{
+	if (x < 0) putchar('-'), write(-x);
+	else if (x / 10) write(x / 10);
+	putchar(x % 10 + '0');
+}
+
+int m, n, k;
+
+struct Worker
+{
+	int l, p, s;
+	bool operator < (Worker B) const {return s < B.s;}
+}w[maxn];
+
+int dp[maxn][maxm], q[maxm];
+
+int calc(int i, int k) {return dp[i-1][k] - w[i].p * k;}
+
+int main()
+{
+#ifdef CraZYali
+	freopen("1821.in", "r", stdin);
+	freopen("1821.out", "w", stdout);
+#endif
+	cin >> m >> n;
+	REP(i, 1, n)
+		scanf("%d%d%d", &w[i].l, &w[i].p, &w[i].s);
+	sort(w + 1, w + 1 + n);
+	REP(i, 1, n)
+	{
+		register int l = 1, r = 0;
+		for (register int k = max(0, w[i].s - w[i].l) ; k <= w[i].s - 1; k++)
+		{
+			while (l <= r && calc(i, q[r]) <= calc(i, k)) r--;
+			q[++r] = k;
+		}
+		REP(j, 1, m)
+		{
+			dp[i][j] = max(dp[i][j-1], dp[i-1][j]);
+			if (j >= w[i].s)
+			{
+				while (l <= r && q[l] < j - w[i].l) l++;
+				if (l <= r) chkmax(dp[i][j], calc(i, q[l]) + w[i].p * j);
+			}
+		}
+	}
+	cout << dp[n][m];
+
+	return 0;
+}
