b219f39ca9e48ed37fd3b1ce9e4ccd76cdc491a9
cnyali-czy <1683053325@qq.com>
2018-09-29 21:28:23 +0800

    终于AC

--- others/POJ/2891.cpp
+++ others/POJ/2891.cpp
@@ -1,20 +1,36 @@
-#define  REP(i, s, e) for(register long long i = s; i <= e ; i++)
-#define DREP(i, s, e) for(register long long i = s; i >= e ; i--)
-#define DEBUG fprintf(stderr, "Passing [%s] in Line %d\n", __FUNCTION__, __LINE__)
+#define  REP(i, s, e) for(register long long i = s; i <= e ;i++)
+#define DREP(i, s, e) for(register long long i = s; i >= e ;i--)
 
-#define chkmax(a, b) a = max(a, b)
+#define DEBUG fprintf(stderr, "Passing [%s] in LINE %lld\n", __FUNCTION__, __LINE__)
 #define chkmin(a, b) a = min(a, b)
+#define chkmax(a, b) a = max(a, b)
 
 #include <iostream>
 #include <cstdio>
-#include <cstdlib>
 #include <cstring>
 
 using namespace std;
 const long long maxn = 1e6 + 10;
 
-long long a[maxn], r[maxn];
+template <typename T> T read()
+{
+	T ans(0), p(1);
+	char c = getchar();
+	while (!isdigit(c))
+	{
+		if (c == '-') p = -1;
+		c = getchar();
+	}
+	while (isdigit(c))
+	{
+		ans = ans * 10 + c - 48;
+		c = getchar();
+	}
+	return ans * p;
+}
+
 long long n;
+long long m[maxn], a[maxn];
 
 long long exgcd(long long a, long long b, long long &x, long long &y)
 {
@@ -24,36 +40,45 @@ long long exgcd(long long a, long long b, long long &x, long long &y)
 		y = 0;
 		return a;
 	}
-	register long long tempans = exgcd(b, a % b, y, x);
+	register long long g = exgcd(b, a % b, y, x);
 	y -= a / b * x;
-	return tempans;
+	return g;
 }
 
-long long solve()
+long long lcm(long long a, long long b)
 {
-	long long M = a[1], R = r[1], x, y;
-	REP(i, 2, n)
-	{
-		register long long d = exgcd(M, a[i], x, y);
-		if ((R - r[i]) % d) return -1ll;
-		x = (R - r[i]) / d * x % a[i];
-		R -= x * M;
-		M = M / d * a[i];
-		R %= M;
-	}
-	return (R % M + M) % M;
+	register long long x, y;
+	return a * b / exgcd(a, b, x, y);
 }
 
-signed main()
+int main()
 {
 #ifdef CraZYali
 	freopen("2891.in", "r", stdin);
 	freopen("2891.out", "w", stdout);
 #endif
-	while(~scanf("%lld",&n))
+	while (scanf("%lld", &n) != EOF)
 	{
-		REP(i, 1, n) scanf("%lld%lld", a + i, r + i);
-		printf("%lld\n", solve());
+		REP(i, 1, n)
+			m[i] = read<long long>(), a[i] = read<long long>();
+		register long long ans = 0 , M = m[1], A = a[1];
+		REP(i, 2, n)
+		{
+			register long long x, y, g;
+			if ((a[i] - A) % (g = exgcd(M, m[i], x, y)))
+			{
+				printf("-1\n");
+				goto next;
+			}
+			x = (a[i] - A) / g * x % (m[i] / g);
+			M = M / g * m[i];
+			A += x * M;
+			A %= M;
+		}
+		printf("%lld\n", (A + M) % M);
+next:;
 	}
+
+
 	return 0;
 }
