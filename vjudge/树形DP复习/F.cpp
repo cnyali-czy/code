03ef88c280fd0227b29f1c5123f0843d145d8fa3
cnyali-czy <1683053325@qq.com>
2018-10-03 20:00:32 +0800

    F

---
+++ vjudge/树形DP复习/F.cpp
@@ -0,0 +1,53 @@
+#define DREP(i, s, e) for(register int i = s; i >= e ;i--)
+#define  REP(i, s, e) for(register int i = s; i <= e ;i++)
+
+#define DEBUG fprintf(stderr, "Passing [%s] in Line %d\n", __FUNCTION__, __LINE__)
+#define chkmax(a, b) a = max(a, b)
+#define chkmin(a, b) a = min(a, b)
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template <typename T> inline T read()
+{
+	T ans(0), p(1);
+	char c = getchar();
+	while (!isdigit(c))
+	{
+		if (c == '-') p = -1;
+		c = getchar();
+	}
+	while (isdigit(c))
+	{
+		ans = ans * 10 + c - 48;
+		c = getchar();
+	}
+	return ans * p;
+}
+
+template <typename T> void write(T x)
+{
+	if (x < 0) putchar('-'), write(-x);
+	else if (x / 10) write(x / 10);
+	putchar(x % 10 + '0');
+}
+
+int m, n, k;
+
+int main()
+{
+#ifdef CraZYali
+	freopen("F.in", "r", stdin);
+	freopen("F.out", "w", stdout);
+#endif
+	while (n = read<int>())
+	{
+		register int Xor = 0;
+		while (n--) Xor ^= read<int>();
+		if (Xor) printf("Rabbit Win!\n");
+		else printf("Grass Win!\n");
+	}
+	
+	return 0;
+}
