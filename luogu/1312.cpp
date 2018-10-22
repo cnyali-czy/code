5619a82e5fe01c7dd7a1c1562e72bd8dee4a0366
cnyali-czy <1683053325@qq.com>
2018-10-22 21:58:13 +0800

    大搜索

--- luogu/1312.cpp
+++ luogu/1312.cpp
@@ -1,107 +1,90 @@
-#define DREP(i, s, e) for(register int i = s; i >= e ;i--)
-#define  REP(i, s, e) for(register int i = s; i <= e ;i++)
-
+#define  REP(i, s, e) for(register int i = s; i <= e ; i++)
+#define DREP(i, s, e) for(register int i = s; i >= e ; i--)
 #define DEBUG fprintf(stderr, "Passing [%s] in Line %d\n", __FUNCTION__, __LINE__)
+
 #define chkmax(a, b) a = max(a, b)
 #define chkmin(a, b) a = min(a, b)
 
-#include <algorithm>
-#include <iostream>
-#include <cstdlib>
-#include <cstring>
-#include <cstdio>
-#include <bitset>
-#include <vector>
-#include <cmath>
-#include <queue>
-#include <map>
-#include <set>
+#include <bits/stdc++.h>
 
 using namespace std;
-const int maxn = 100010;
 
-template <typename T> T read()
+int n, m, k;
+int num[15], i[10][10], ans[6][3];
+
+int Remove(int i[10][10])
 {
-	T ans(0), p(1);
-	char c = getchar();
-	while (!isdigit(c)) {
-		if (c == '-') p = -1;
-		c = getchar();
-	}
-	while (isdigit(c)) {
-		ans = ans * 10 + c - 48;
-		c = getchar();
-	}
-	return ans * p;
+	register int flag = 0, f[10][10] = {0};
+	REP(a, 1, 5)
+		REP(b, 1, 7)
+		if (i[a][b])
+		{
+			if (a <= 3 && i[a][b] == i[a+1][b] && i[a+1][b] == i[a+2][b]) f[a][b] = f[a+1][b] = f[a+2][b] = 1;
+			if (b <= 5 && i[a][b] == i[a][b+1] && i[a][b+1] == i[a][b+2]) f[a][b] = f[a][b+1] = f[a][b+2] = 1;
+		}
+	REP(a, 1, 5)
+		REP(b, 1, 7)
+		if (f[a][b])
+		{
+			i[a][b] = 0;
+			flag = 1;
+		}
+	return flag;
 }
-int m, n, k;
-
-struct Graph
+void Fall(int i[10][10])
 {
-	int co[10][10];
-}X;
+	REP(a, 1, 5)
+	{
+		register int pos = 0;
+		REP(b, 1, 7)
+		{
+			register int temp = i[a][b];
+			i[a][b] = 0;
+			if (temp) i[a][++pos] = temp;
+		}
+	}
+}
 
-#define node pair<Graph, vector <pair<pair<int, int>, int> > >
-
-inline bool judge(node T)
+int Check(int i[10][10])
 {
-	REP(i, 1, 5)
-		REP(j, 1, 7)
-			if (T.first.co[i][j]) return 0;
+	REP(a, 1, 5)
+		REP(b, 1, 7) if (i[a][b]) return 0;
 	return 1;
 }
 
-inline void out(node T)
+void dfs(int A, int i[10][10])
 {
-	int siz = T.second.size() - 1;
-	REP(i, 0, siz)
-		printf("%d %d %d\n", T.second[i].first.first, T.second[i].first.second, T.second[i].second);
-}
-
-queue <pair<Graph, node> > q;
-
-map <node, bool> vis;
-
-inline void fall(node &T)
-{
-	REP(i, 1, 5)
-		REP(j, 1, 7)
-		{
-			int cnt = 0;
-			if (T.first.co[i][j]) T.first.co[i][++cnt] = T.first.co[i][j];
-			while (cnt < 7)
-			{
-				cnt++;
-				T.first.co[i][cnt] = 0;
-			}
-		}
-}
-
-bool mark[10][10];
-inline int del(node &T)
-{
-	bool flag = 0;
-	memset(mark, 0, sizeof(mark));
-	REP(i, 1, 5)
-		REP(j, 1, 5)
-			if (T.first.co[i][j] == T.first.co[i][j + 1] && T.first.co[i][j + 1] == T.first.co[i][j + 2])
-			{
-				flag = 1;
-				mark[i][j] = mark[i][j+1] = mark[i][j+2] = 1;
-			}
-	REP(i, 1, 3)
-		REP(j, 1, 7)
-			if (T.first.co[i][j] == T.first.co[i + 1][j] && T.first.co[i + 1][j] == T.first.co[i + 2][j])
-			{
-				flag = 1;
-				mark[i][j] = mark[i+1][j] = mark[i+2][j] = 1;
-			}
-	if (!flag) return -1;
-	else
+	if (A == n)
 	{
-		fall(T);
-		return 1;
+		if (Check(i))
+		{
+			REP(a, 1, n)
+				if (ans[a][2]) printf("%d %d -1\n", ans[a][0], ans[a][1] - 1);
+				else printf("%d %d 1\n", ans[a][0] - 1, ans[a][1] - 1);
+			exit(0);
+		}
+		return;
 	}
+	A++;
+	memset(num, 0, sizeof(num));
+	REP(a, 1, 5)
+		REP(b, 1, 7) num[i[a][b]]++;
+	REP(i, 1, 10)
+		if (num[i] == 1 || num[i] == 2) return;
+	int f[10][10] = {0};
+	REP(a, 1, 4)
+		REP(b, 1, 7)
+		if (i[a][b] != i[a + 1][b])
+		{
+			memcpy(f, i, sizeof(f));
+			ans[A][0] = a;
+			ans[A][1] = b;
+			ans[A][2] = (i[a][b] ? 0 : 1);
+			swap(f[a][b], f[a+1][b]);
+			Fall(f);
+			while (Remove(f)) Fall(f);
+			dfs(A, f);
+		}
 }
 
 int main()
@@ -110,69 +93,15 @@ int main()
 	freopen("1312.in", "r", stdin);
 	freopen("1312.out", "w", stdout);
 #endif
-	n = read<int>();
-	REP(i, 1, 5)
-	{
-		int cnt = 0;
-		while (1)
+	cin >> n;
+	REP(a, 1, 5)
+		REP(b, 1, 1e9)
 		{
-			k = read<int>();
-			if (!k) break;
-			X.co[i][++cnt] = k;
+			scanf("%d", &i[a][b]);
+			if (!i[a][b]) break;
 		}
-	}
-	vector <pair<pair<int, int>, int> > emptyV;
-	q.push(make_pair(X, emptyV));
-
-
-	while (!q.empty())
-	{
-		node T = q.front();
-		q.pop();
-		if (vis[T] && T != make_pair(X, emptyV)) continue;
-		else if (T.second.size() > n)
-		{
-			cout << -1;
-			return 0;
-		}
-		vis[T] = 1;
-		if (judge(T))
-		{
-			out(T);
-			return 0;
-		}
-		REP(i, 1, 5)
-			REP(j, 1, 7)
-				if (!T.first.co[i][j]) break;
-				else
-				{
-					if (i < 5 && T.first.co[i][j] ^ T.first.co[i+1][j])
-					{
-						node temp = T;
-						swap(temp.first.co[i][j], temp.first.co[i+1][j]);
-						fall(temp);
-						while (1)
-						{
-							int re = del(T);
-							if (re == -1) break;
-						}
-						temp.second.push_back(make_pair(make_pair(i, j), 1));
-					}
-					if (i > 1 && T.first.co[i][j] ^ T.first.co[i-1][j])
-					{
-						node temp = T;
-						swap(temp.first.co[i][j], temp.first.co[i-1][j]);
-						fall(temp);
-						while (1)
-						{
-							register int re = del(T);
-							if (re == -1) break;
-						}
-						temp.second.push_back(make_pair(make_pair(i, j), -1));
-						q.push(temp);
-					}
-				}
-	}
+	dfs(0, i);
 	cout << -1;
 	return 0;
 }
+
