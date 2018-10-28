1e535236bb246f5b859de9ac0c9dc71bac57cb9b
cnyali-czy <1683053325@qq.com>
2018-10-28 22:44:58 +0800

    kmp

--- luogu/3375.cpp
+++ luogu/3375.cpp
@@ -5,26 +5,39 @@
 #define chkmax(a, b) a = max(a, b)
 #define chkmin(a, b) a = min(a, b)
 
-#include <algorithm>
-#include <iostream>
-#include <cstdlib>
-#include <cstring>
-#include <cstdio>
-#include <bitset>
-#include <vector>
-#include <cmath>
-#include <queue>
-#include <map>
-#include <set>
+#include <bits/stdc++.h>
 
 using namespace std;
-const int maxlen = 1000000 + 10;
+const int maxn = 1000000 + 10, maxm = 1000000 + 10;
+
+template <typename T> inline T read()
+{
+	T ans(0), p(1);
+	char c = getchar();
+	while (!isdigit(c))
+	{
+		if (c == '-') p = -1;
+		c = getchar();
+	}
+	while (isdigit(c))
+	{
+		ans = ans * 10 + c - 48;
+		c = getchar();
+	}
+	return ans * p;
+}
+
+template <typename T> void write(T x)
+{
+	if (x < 0) putchar('-'), write(-x);
+	else if (x / 10) write(x / 10);
+	putchar(x % 10 + '0');
+}
 
 int m, n, k;
-char s1[maxlen], s2[maxlen];
-int len1, len2;
+char a[maxn], b[maxm];
 
-int nxt[maxlen];
+int ne[maxm];
 
 int main()
 {
@@ -32,27 +45,21 @@ int main()
 	freopen("3375.in", "r", stdin);
 	freopen("3375.out", "w", stdout);
 #endif
-	scanf("%s\n%s", s1 + 1, s2 + 1);
-	len1 = strlen(s1 + 1);
-	len2 = strlen(s2 + 1);
-
-	register int j = 0;
-	REP(i, 2, len2)
+	scanf("%s\n%s", a + 1, b + 1);
+	n = strlen(a + 1);m = strlen(b + 1);
+	k = 0;
+	REP(i, 2, m)
 	{
-		while (j > 0 && s2[i] ^ s2[j + 1]) j = nxt[j];
-		if (s2[i] == s2[j + 1]) ++j;
-		nxt[i] = j;
+		while (k > 0 && b[k + 1] != b[i]) k = ne[k];
+		ne[i] = k += b[i] == b[k + 1];
 	}
-
-	j = 0;
-	REP(i, 1, len1)
+	k = 0;
+	REP(i, 1, n)
 	{
-		while (j > 0 && s1[i] ^ s2[j + 1]) j = nxt[j];
-		if (s1[i] == s2[j + 1]) j++;
-		if (j == len2) printf("%d\n", i - len2 + 1);
+		while (k > 0 && b[k + 1] != a[i]) k = ne[k];
+		if (b[k + 1] == a[i]) k++;
+		if (k == m) printf("%d\n", i - m + 1);
 	}
-
-	REP(i, 1, len2) printf("%d ", nxt[i]);
-
+	REP(i, 1, m) printf("%d ", ne[i]);
 	return 0;
 }
