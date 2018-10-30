191d830f90c556c6bdc2bbbfb27ce2c295278b53
cnyali-czy <1683053325@qq.com>
2018-10-30 09:10:15 +0800

    nice

---
+++ luogu/2197.cpp
@@ -0,0 +1,55 @@
+#define DREP(i, s, e) for(register int i = s; i >= e ;i--)
+#define  REP(i, s, e) for(register int i = s; i <= e ;i++)
+
+#define DEBUG fprintf(stderr, "Passing [%s] in Line %d\n", __FUNCTION__, __LINE__)
+#define chkmax(a, b) a = max(a, b)
+#define chkmin(a, b) a = min(a, b)
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template <typename T> inline T read()
+{
+	T ans(0), p(1);
+	char c = getchar();
+	while (!isdigit(c))
+	{
+		if (c == '-') p = -1;
+		c = getchar();
+	}
+	while (isdigit(c))
+	{
+		ans = ans * 10 + c - 48;
+		c = getchar();
+	}
+	return ans * p;
+}
+
+template <typename T> void write(T x)
+{
+	if (x < 0) putchar('-'), write(-x);
+	else if (x / 10) write(x / 10);
+	putchar(x % 10 + '0');
+}
+
+int m, n, k;
+
+int main()
+{
+#ifdef CraZYali
+	freopen("2197.in", "r", stdin);
+	freopen("2197.out", "w", stdout);
+#endif
+
+	register int T = read<int>();
+	while (T --> 0)
+	{
+		register int n = read<int>();k =  0;
+		while (n--) k ^= read<int>();
+		if (k) printf("Yes\n");
+		else printf("No\n");
+	}
+	
+	return 0;
+}
