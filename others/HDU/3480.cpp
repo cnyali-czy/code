acaa9270ddaa6a2550891b2efbd6057609985baf
cnyali-czy <1683053325@qq.com>
2019-04-03 11:27:47 +0800

    update

--- others/HDU/3480.cpp
+++ others/HDU/3480.cpp
@@ -29,10 +29,11 @@ template <typename T> T read()
 	return ans * p;
 }
 
-int n, m, a[maxn];
-int dp[maxn][maxm], s[maxn][maxm];
+int n, m, a[maxn], s[maxm][maxn];
 
 int cost(int i, int j) {return (a[i]-a[j]) * (a[i]-a[j]);}
+
+int dp[maxm][maxn];
 
 int main()
 {
@@ -46,18 +47,17 @@ int main()
 		n = read<int>();m = read<int>();
 		REP(i, 1, n) a[i] = read<int>();
 		sort(a + 1, a + 1 + n);
-		REP(i, 1, n) dp[i][1] = cost(1, i);
-		REP(i, 1, n) s[n+1][i] = n;
-		REP(j, 2, m)
-			DREP(i, n, 1)
+		REP(i, 1, n) dp[1][i] = cost(s[1][i] = 1, i);
+		REP(i, 2, m) s[i][n+1] = n-1;
+		REP(i, 2, m)
+			DREP(j, n, 1)
 			{
-				int &ans = dp[i][j];
-				ans = inf;
-				REP(k, s[i][j-1], s[i+1][j])
-					if (ans > dp[k-1][j-1] + cost(k, i))
-						ans = dp[k-1][j-1] + cost(s[i][j] = k, i);
+				int &ans = dp[i][j] = inf;
+				REP(k, s[i-1][j], s[i][j+1])
+					if (ans > dp[i-1][k] + cost(k+1, j))
+						ans = dp[i-1][s[i][j] = k] + cost(k+1, j);
 			}
-		printf("Case %d: %d\n", Case, dp[n][m]);
+		printf("Case %d: %d\n", Case, dp[m][n]);
 	}
 	return 0;
 }
