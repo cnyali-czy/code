7094bf2fa208d5cc99fec06c0634f456a1800458
cnyali-czy <cnyali-czy@1683053325@qq.com>
2019-07-19 20:21:17 +0800

    better

--- lab/day5/evening/A.cpp
+++ lab/day5/evening/A.cpp
@@ -13,25 +13,6 @@ const int maxn = 1e6 + 10, maxL = 4200000;
 
 int n, ans, a[maxn], b[maxn], cnt[3000005];
 int s[3000005];
-void add(int x)
-{
-	while (x <= 3000001)
-	{
-		s[x]++;
-		x += x & -x;
-	}
-}
-int sum(int x)
-{
-	int res = 0;
-	while (x > 0)
-	{
-		res += s[x];
-		x -= x & -x;
-	}
-	return res;
-}
-
 signed main()
 {
 #ifndef ONLINE_JUDGE
@@ -50,7 +31,8 @@ signed main()
 	else
 	{
 		REP(i, 1, n) cnt[a[i]]++;
-		REP(i, 1, n) add(b[i]);
+		REP(i, 1, n) s[b[i]]++;
+		REP(i,1,3000001)s[i]+=s[i-1];
 		REP(A, 1, 3000001)
 			if (cnt[A])
 				REP(C, 1, 1732)
@@ -61,14 +43,14 @@ signed main()
 					{
 						chkmax(l, 1ll);chkmin(l, 3000001ll);
 						chkmax(r, 1ll);chkmin(r, 3000001ll);
-						ans += C * cnt[A] * (sum(r) - sum(l-1));
+						ans += C * cnt[A] * (s[r] - s[l-1]);
 					}
 					l = A + C * C, r = A + C * C + C * 2;
 					if (max(l, 1ll) <= min(r, 3000001ll))
 					{
 						chkmax(l, 1ll);chkmin(l, 3000001ll);
 						chkmax(r, 1ll);chkmin(r, 3000001ll);
-						ans += C * cnt[A] * (sum(r) - sum(l-1));
+						ans += C * cnt[A] * (s[r] - s[l-1]);
 					}
 				}
 	}
