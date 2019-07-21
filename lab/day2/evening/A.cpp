1be722884eb0fb33a63c5075b6750001f019bfdf
cnyali-czy <cnyali-czy@1683053325@qq.com>
2019-07-21 22:43:51 +0800

    左偏树

--- lab/day2/evening/A.cpp
+++ lab/day2/evening/A.cpp
@@ -1,13 +1,14 @@
-#define REP(i, s, e) for (int i = s; i <= e; i++)
-#define DEBUG fprintf(stderr, "Passing [%s] in Line %lld\n", __FUNCTION__, __LINE__)
+#define REP(i, s, e) for (int i = s; i <= e ; i++)
+#define DEBUG fprintf(stderr, "Passing [%s] in Line %d\n", __FUNCTION__, __LINE__)
 
-#include <bits/extc++.h>
+#include <iostream>
+#include <cstdio>
 #define int long long
 
 using namespace std;
-const int maxn = 300000 + 10, maxm = maxn;
+const int maxn = 300000 + 10, maxm = maxn, inf = 1ll << 40;
 
-int bg[maxn], ne[maxn << 1], to[maxn << 1], e;
+int bg[maxn], ne[maxm << 1], to[maxm << 1], e;
 void add(int x, int y)
 {
 	e++;
@@ -15,47 +16,97 @@ void add(int x, int y)
 	ne[e] = bg[x];
 	bg[x] = e;
 }
-int depth[maxn], fa[maxn];
-void pre(int x)
+int depth[maxn];
+void dfs(int x, int fa = -1)
 {
 	for (int i = bg[x]; i ; i = ne[i])
-		if (to[i] ^ fa[x])
+		if (to[i] ^ fa)
 		{
-			fa[to[i]] = x;
 			depth[to[i]] = depth[x] + 1;
-			pre(to[i]);
+			dfs(to[i], x);
 		}
 }
 
-struct man
+struct worker
 {
-	int down, up, cost, id;
-	man(){}
-	man(int _down, int _up, int _cost, int _id) : down(_down), up(_up), cost(_cost), id(_id) {}
-	bool operator < (man B) const {return cost > B.cost || cost == B.cost && depth[down] - depth[up] < depth[B.down] - depth[B.up] || cost == B.cost && depth[down] - depth[up] == depth[B.down] - depth[B.up] && id == B.id;}
-};
-
-__gnu_pbds :: priority_queue <man> q[maxn], del[maxn], temp;
-
-int Id[maxn];
-bool notleave[maxn];
-int solve(int x)
+	int up, w, ne;
+}p[maxm << 1];
+int cur = 300000;
+void insert(int x, int up, int w)
 {
-	for (int i = bg[x]; i ; i = ne[i])
-		if (to[i] ^ fa[x])
+	p[++cur] = (worker){up, w, p[x].ne};
+	p[x].ne = cur;
+}
+
+struct heap *null;
+struct heap
+{
+	int up, f, Min, tag, dis;
+	heap *l, *r;
+	void pushdown()
+	{
+		if (tag)
 		{
-			notleave[x] = 1;
-			solve(to[i]);
-			q[x].join(q[to[i]]);
-			del[x].join(del[to[i]]);
+			f += tag;
+			Min += tag;
+			l -> tag += tag;
+			r -> tag += tag;
+			tag = 0;
 		}
-	while (!del[x].empty() && del[x].top().id == q[x].top().id) q[x].pop(), del[x].pop();
-	if (q[x].empty() && x > 1) {cout << -1;exit(0);}
-	//temp=q[x];
-	//while(!temp.empty()){printf("%lld %lld %lld\n", x, temp.top().cost, temp.top().id);temp.pop();}
-	return Id[x] = (!q[x].empty() ? q[x].top().id : -1);
+	}
+	void pushup()
+	{
+		Min = min(f, min(l -> Min + l -> tag, r -> Min + r -> tag));
+		if (l -> dis < r -> dis) swap(l, r);
+		dis = r -> dis + 1;
+	}
+}pool[maxn], *now = pool, *root[maxn];
+heap* merge(heap *x, heap *y)
+{
+	if (x == null) return y;
+	if (y == null) return x;
+	x -> pushdown();y -> pushdown();
+	if (x -> up <= y -> up) swap(x, y);
+	x -> r = merge(x -> r, y);
+	x -> pushup();
+	return x;
 }
-int n, m, cost[maxm];
+void solve(int x, int fa = -1)
+{
+	int sum = 0;
+	for (int i = bg[x]; i ; i = ne[i])
+		if (to[i] ^ fa)
+		{
+			solve(to[i], x);
+			while (root[to[i]] -> up >= depth[to[i]])
+				root[to[i]] = merge(root[to[i]] -> l, root[to[i]] -> r);
+			if (root[to[i]] == null)
+			{
+				cout << -1 << endl;
+				exit(0);
+			}
+			sum += root[to[i]] -> Min;
+		}
+	for (int i = p[x].ne; i ; i = p[i].ne)
+	{
+		*now = (heap) {p[i].up, sum + p[i].w, sum + p[i].w, 0, 1, null, null};
+		root[x] = merge(root[x], now++);
+	}
+	for (int i = bg[x]; i ; i = ne[i])
+		if (to[i] ^ fa)
+		{
+			int Min = root[to[i]] -> Min;
+			root[to[i]] -> tag += sum - Min;
+			root[x] = merge(root[x], root[to[i]]);
+		}
+	if (root[x] == null)
+	{
+		cout << -1 << endl;
+		exit(0);
+	}
+}
+
+int m, n, k;
 
 signed main()
 {
@@ -70,19 +121,19 @@ signed main()
 		scanf("%lld%lld", &x, &y);
 		add(x, y);add(y, x);
 	}
-	pre(1);
+	depth[1] = 1;
+	dfs(1);
 	REP(i, 1, m)
 	{
-		int down, up;
-		scanf("%lld%lld%lld", &down, &up, cost + i);
-		q[down].push(man(down, up, cost[i], i));
-		del[up].push(man(down, up, cost[i], i));
+		int u, v, w;
+		scanf("%lld%lld%lld", &u, &v, &w);
+		insert(u, depth[v], w);
 	}
+	null = new heap;
+	null -> l = null -> r = null;
+	null -> Min = inf;
+	REP(i, 1, n) root[i] = null;
 	solve(1);
-//	REP(i,1,n)cout<<Id[i]<<' ';cout<<endl;
-	int ans = 0;
-	REP(i,2,n)
-		if(Id[fa[i]] != Id[i]) ans += cost[Id[i]];
-	cout << ans << endl;
+	printf("%lld\n", root[1] -> Min + root[1] -> tag);
 	return 0;
 }
