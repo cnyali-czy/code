41ae2b51e4c28462d1144532406414b6180a92ea
cnyali-czy <1683053325@qq.com>
2019-08-01 20:31:54 +0800

    推式子找规律

---
+++ luogu/3978.cpp
@@ -0,0 +1,41 @@
+#define DREP(i, s, e) for(int i = s; i >= e ;i--)
+#define  REP(i, s, e) for(int i = s; i <= e ;i++)
+
+#define DEBUG fprintf(stderr, "Passing [%s] in Line %d\n", __FUNCTION__, __LINE__)
+#define chkmax(a, b) a = max(a, b)
+#define chkmin(a, b) a = min(a, b)
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template <typename T> inline T read()
+{
+	T ans(0), p(1);
+	char c = getchar();
+	while (!isdigit(c))
+	{
+		if (c == '-') p = -1;
+		c = getchar();
+	}
+	while (isdigit(c))
+	{
+		ans = ans * 10 + c - 48;
+		c = getchar();
+	}
+	return ans * p;
+}
+
+int n, m, k;
+
+int main()
+{
+#ifdef CraZYali
+	freopen("3978.in", "r", stdin);
+	freopen("3978.out", "w", stdout);
+#endif
+	n = read<int>();
+	printf("%.10lf\n", 1. * n * (n+1) / 2 / (2 * n - 1));
+
+	return 0;
+}
