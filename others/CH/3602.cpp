1fa12248b91ce92b1b11b6fa3b60965fc84d46ac
cnyali-czy <yms-chenziyang@outlook.com>
2019-09-09 22:40:07 +0800

    useless

--- others/CH/3602.cpp
+++ others/CH/3602.cpp
@@ -1,12 +1,11 @@
-#define DREP(i, s, e) for(register int i = s; i >= e ;i--)
-#define  REP(i, s, e) for(register int i = s; i <= e ;i++)
+#define DREP(i, s, e) for (int i(s), end_##i(e); i >= end_##i ;i--)
+#define  REP(i, s, e) for (int i(s), end_##i(e); i <= end_##i ;i++)
 
 #define DEBUG fprintf(stderr, "Passing [%s] in Line %d\n", __FUNCTION__, __LINE__)
 #define chkmax(a, b) a = max(a, b)
 #define chkmin(a, b) a = min(a, b)
 
 #include <bits/stdc++.h>
-
 #define int long long
 
 using namespace std;
@@ -47,7 +46,7 @@ template <typename T> void write(T x)
 
 int m, n, k;
 
-int low[maxn], pre[maxn], dfs_clock;
+int low[maxn], dfn[maxn], dfs_clock;
 stack <int> S;
 
 int belong[maxn], size[maxn], cnt;
@@ -55,17 +54,17 @@ bool vis[maxn];
 
 void dfs(int u)
 {
-	low[u] = pre[u] = ++dfs_clock;
+	low[u] = dfn[u] = ++dfs_clock;
 	vis[u] = 1;
 	S.push(u);
-	for (register int i = bg[u]; i ; i = ne[i])
-		if (!pre[to[i]])
+	for (int i = bg[u]; i ; i = ne[i])
+		if (!dfn[to[i]])
 		{
 			dfs(to[i]);
 			chkmin(low[u], low[to[i]]);
 		}
-		else if (vis[to[i]]) chkmin(low[u], pre[to[i]]);
-	if (low[u] == pre[u])
+		else if (vis[to[i]]) chkmin(low[u], dfn[to[i]]);
+	if (low[u] == dfn[u])
 	{
 		belong[u] = ++cnt;
 		vis[u] = 0;
@@ -86,49 +85,32 @@ void init()
 	e = 0;
 	REP(i, 1, n) bg[i] = 0;
 
-	REP(i, 1, n) belong[i] = pre[i] = low[i] = vis[i] = 0;
-	cnt = 0;
-	dfs_clock = 0;
+	REP(i, 1, n) belong[i] = dfn[i] = low[i] = vis[i] = 0;
+	cnt = dfs_clock = 0;
 }
 
 void tarjan()
 {
-	REP(i, 1, n)
-		if (!pre[i]) dfs(i);
-}
-
-void tarjan_test()
-{
-	cin >> n >> m;
-	while (m --> 0)
-	{
-		register int x = read<int>(), y = read<int>();
-		add(x, y);
-	}
-	tarjan();
-	REP(i, 1, n)
-		printf("%d%c", belong[i], i == n ? '\n' : ' ');
+	REP(i, 1, n) if (!dfn[i]) dfs(i);
 }
 
 int l[maxn];
 
-int power_pow(int a, int b)
+inline int power_pow(int base, int b)
 {
-	a %= MOD;a = (a + MOD) % MOD;
-	int base = a, ans = 1;
+	int ans(1);
 	while (b)
 	{
-		if (b & 1) ans = ans * base % MOD;
-		base = base * base % MOD;
+		if (b & 1) (ans *= base) %= MOD;
+		(base *= base) %= MOD;
 		b >>= 1;
 	}
-	return (ans + MOD) % MOD;
+	return ans;
 }
+#define inv(x) power_pow(x, MOD - 2)
+#define F(x) power_pow((x), (x) - 2)
 
-int inv(int x) {return power_pow(x, MOD - 2);}
-int F(int x) {return power_pow(x, x - 2);}
-
-int Inv[maxn];
+int fac[maxn], Inv[maxn];
 
 signed main()
 {
@@ -136,23 +118,19 @@ signed main()
 	freopen("3602.in", "r", stdin);
 	freopen("3602.out", "w", stdout);
 #endif
-	register int T = read<int>();
-	
-	register int temp = 1;
-	REP(i, 1, 100000)
-	{
-		temp = temp * i % MOD;
-		Inv[i] = inv(temp);
-	}
+	int T = read<int>();
+
+	fac[0] = 1;
+	REP(i, 1, 100000) fac[i] = fac[i-1] * i % MOD;
+	Inv[100000] = inv(fac[100000]);
+	DREP(i, 99999, 0) Inv[i] = Inv[i+1] * (i+1) % MOD;
 
 	while (T --> 0)
 	{
 		n = read<int>();
-		
-		init();
-		REP(i, 1, n)
-			add(l[i] = read<int>(), i);
 
+		init();
+		REP(i, 1, n) add(l[i] = read<int>(), i);
 		tarjan();
 		REP(i, 1, n) size[belong[i]]++;
 		int ans = 1;
@@ -161,6 +139,6 @@ signed main()
 			if (size[i] > 1) ans = ans * F(size[i]) % MOD * Inv[size[i] - 1] % MOD;
 		printf("%lld\n", (ans + MOD) % MOD);
 	}
-	
+
 	return 0;
 }
