ab1349dcca6377a51e91dac27328d80907debc58
cnyali-czy <1683053325@qq.com>
2019-12-17 10:22:19 +0800

    多项式ln

---
+++ luogu/4725.cpp
@@ -0,0 +1,145 @@
+#define REP(i, s, e) for (register int i = (s), end_##i = (e); i <= end_##i; i++)
+#define DEP(i, s, e) for (register int i = (s), end_##i = (e); i >= end_##i; i--)
+#define DEBUG fprintf(stderr, "Passing [%s] in Line %d\n", __FUNCTION__, __LINE__)
+
+#define chkmax(a, b) (a < (b) ? a = (b) : a)
+#define chkmin(a, b) (a > (b) ? a = (b) : a)
+#include <algorithm>
+#include <iostream>
+#include <cassert>
+#include <cstring>
+#include <cstdlib>
+#include <cstdio>
+#include <vector>
+#include <cmath>
+using namespace std;
+const int maxn = 1e5 + 10, MOD = 998244353;
+
+int power_pow(long long base, int b)
+{
+	long long ans(1);
+	while (b)
+	{
+		if (b & 1) (ans *= base) %= MOD;
+		(base *= base) %= MOD;
+		b >>= 1;
+	}
+	return ans;
+}
+#define inv(x) power_pow(x, MOD - 2)
+
+template <typename T> T read()
+{
+	T ans(0), flag(1);
+	char c(getchar());
+	while (!isdigit(c))
+	{
+		if (c == '-') flag = -1;
+		c = getchar();
+	}
+	while (isdigit(c))
+	{
+		ans = ans * 10 + c - 48;
+		c = getchar();
+	}
+	return ans * flag;
+}
+
+void file(string s)
+{
+	freopen((s + ".in").c_str(), "r", stdin);
+	freopen((s + ".out").c_str(), "w", stdout);
+}
+
+int m, n, k;
+
+vector <int> DIF(vector <int> F)
+{
+	if (F.size() == 1) return vector<int>(1, 0);
+	vector <int> res(F.size() - 1);
+	REP(i, 0, res.size() - 1)
+		res[i] = (i + 1ll) * F[i+1] % MOD;
+	return res;
+}
+const int qaq = 1 << 18;
+int Inv[qaq + 5];
+struct __init__
+{
+	__init__(const int n = qaq)
+	{
+		Inv[0] = Inv[1] = 1;
+		REP(i, 2, n) Inv[i] = -1ll * (MOD / i) * Inv[MOD % i] % MOD;
+	}
+}__INIT__;
+vector <int> INT(vector <int> F)
+{
+	vector <int> res(F.size() + 1);
+	res[0] = 0;
+	REP(i, 1, F.size())
+		res[i] = 1ll * F[i-1] * Inv[i] % MOD;
+	return res;
+}
+
+void NTT(vector <int> &a, int L, int flag)
+{
+	const int n = 1 << L;
+	a.resize(n);
+	vector <int> R(n);
+	R[0] = 0;
+	REP(i, 1, n - 1)
+	{
+		R[i] = (R[i >> 1] >> 1) | ((i & 1) << L - 1);
+		if (i < R[i]) swap(a[i], a[R[i]]);
+	}
+	for (int i = 1; i < n; i <<= 1)
+		for (int k = 0; k < n; k += i << 1)
+		{
+			int wn = power_pow(3, (MOD - 1) / (i << 1));
+			if (flag < 0) wn = inv(wn);
+			for (int l = 0, w = 1; l < i; l++, w = 1ll * w * wn % MOD)
+			{
+				int x(a[k + l]), y(1ll * a[k + l + i] * w % MOD);
+				a[k + l] = (x + y) % MOD;
+				a[k + l + i] = (x - y) % MOD;
+			}
+		}
+	if (flag < 0)
+		REP(i, 0, n - 1) a[i] = 1ll * a[i] * Inv[n] % MOD;
+}
+
+vector <int> getInv(vector <int> f, int L)
+{
+	if (!L) return vector<int>(1, inv(f[0]));
+	const int n = 1 << L;
+	vector <int> A(n + n, 0), B(getInv(f, L - 1));
+	f.resize(n);
+	NTT(f, L + 1, 1);
+	NTT(B, L + 1, 1);
+	REP(i, 0, n + n - 1) A[i] = 1ll * B[i] * (2 - 1ll * f[i] * B[i] % MOD) % MOD;;
+	NTT(A, L + 1, -1);
+	A.resize(n);
+	return A;
+}
+
+vector <int> f;
+
+signed main()
+{
+#ifdef CraZYali
+	file("4725");
+#endif
+	cin >> n;
+	int l(1), L(0);
+	while (l <= n + n) l <<= 1, L++;
+	f.resize(l);
+	REP(i, 0, n - 1) f[i] = read<int>();
+	vector <int> Inv(getInv(f, L-1)), res(l,0);
+	f = DIF(f);
+	NTT(Inv, L, 1);
+	NTT(f, L, 1);
+	REP(i, 0, l - 1) res[i] = 1ll * Inv[i] * f[i] % MOD;
+	NTT(res, L, -1);
+	res = INT(res);
+	REP(i, 0, n-1) printf("%d%c", (res[i] + MOD) % MOD, i == n - 1 ? '\n' : ' ');
+	return 0;
+}
