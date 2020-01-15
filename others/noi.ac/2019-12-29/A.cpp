7635b28466b020f6925ab79315eef57332efd466
cnyali-czy <1683053325@qq.com>
2020-01-15 16:33:53 +0800

    QAQ

---
+++ others/noi.ac/2019-12-29/A.cpp
@@ -0,0 +1,53 @@
+#define REP(i, s, e) for (register int i = (s), end_##i = (e); i <= end_##i; i++)
+#define DEP(i, s, e) for (register int i = (s), end_##i = (e); i >= end_##i; i--)
+#define DEBUG fprintf(stderr, "Passing [%s] in Line %d\n", __FUNCTION__, __LINE__)
+
+#define chkmax(a, b) (a < (b) ? a = (b) : a)
+#define chkmin(a, b) (a > (b) ? a = (b) : a)
+
+#include <algorithm>
+#include <iostream>
+#include <cassert>
+#include <cstring>
+#include <cstdlib>
+#include <cstdio>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+template <typename T> T read()
+{
+	T ans(0), flag(1);
+	char c(getchar());
+	while (!isdigit(c))
+	{
+		if (c == '-') flag = -1;
+		c = getchar();
+	}
+	while (isdigit(c))
+	{
+		ans = ans * 10 + c - 48;
+		c = getchar();
+	}
+	return ans * flag;
+}
+
+void file(string s)
+{
+	freopen((s + ".in").c_str(), "r", stdin);
+	freopen((s + ".out").c_str(), "w", stdout);
+}
+
+int m, n, k;
+
+int main()
+{
+#ifdef CraZYali
+	file("t1");
+#endif
+	cin >> n >> k;
+	cout << n << endl;
+	while (k--) putchar('1'), putchar(10);
+	return 0;
+}
