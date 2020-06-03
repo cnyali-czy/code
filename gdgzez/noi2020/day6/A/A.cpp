6494b0208fb3a5abe4be45a0626bd473dbcfbdcc
cnyali-czy <1683053325@qq.com>
2020-06-03 16:31:24 +0800

    暴力出奇迹

--- gdgzez/noi2020/day6/A/A.cpp
+++ gdgzez/noi2020/day6/A/A.cpp
@@ -73,45 +73,7 @@ inline int min(int x, int y) {if (x < y) return x;return y;}
 #define poly vector <int>
 #define i64 long long
 #define maxn maxN
-const int maxN = 1 << 21;
-namespace Less
-{
-	int II;
-	struct num
-	{
-		int x, y;
-		inline num(int x = 0, int y = 0) : x(x), y(y) {}
-		inline num operator * (num B) {return num(add(1ll * x * B.x % MOD, 1ll * II * y % MOD * B.y % MOD), add(1ll * x * B.y % MOD, 1ll * y * B.x % MOD));}
-		inline num operator *=(num B) {return *this = *this * B;}
-	};
-
-	num power_pow(num base, int b)
-	{
-		num ans(1);
-		while (b)
-		{
-			if (b & 1) ans *= base;
-			base *= base;
-			b >>= 1;
-		}
-		return ans;
-	}
-
-	mt19937 hh;
-	int solve(int n)
-	{
-		if (n == 1) return 1;
-		int a = MOD - 1;
-		while (1)
-		{
-			II = sub(1ll * a * a % MOD, n);
-			if (a && ::power_pow(II, MOD - 1 >> 1) == MOD - 1) break;
-			a = hh() % MOD;
-		}
-		int ans1 = power_pow(num(a, 1), MOD + 1 >> 1).x, ans2 = sub(0, ans1);
-		return min(ans1, ans2);
-	}
-}
+const int maxN = 1 << 18;
 
 inline int deg(const poly &A) {return A.size() - 1;}
 unsigned i64 NTTtmp[maxn];
@@ -121,7 +83,7 @@ struct __init__
 {
 	__init__()
 	{
-		REP(i, 0, 19)
+		REP(i, 0, 18)
 		{
 			int Wn = power_pow(3, (MOD - 1) / (1 << i + 1));
 			int InvWn = inv(Wn);
@@ -185,116 +147,6 @@ inline poly&operator *=(poly&a, poly b)
 	NTT(a, l, -1);
 	a.resize(n + m + 1);
 	return a;
-}
-inline poly Inv(const poly &F)
-{
-	poly A(1, inv(F[0]));
-	int l = 1, n(deg(F));
-	for (int d = 2; d <= n + n; d <<= 1)
-	{
-		poly H(d, 0);
-		REP(i, 0, min(n, d - 1)) H[i] = F[i];
-		NTT(H, d + d, 1);
-		NTT(A, d + d, 1);
-		REP(i, 0, d + d - 1) A[i] = A[i] * (2 + MOD - 1ll * A[i] * H[i] % MOD) % MOD;
-		NTT(A, d + d, -1);
-		A.resize(d);
-	}
-	A.resize(n + 1);
-	return A;
-}
-int invs[maxn], lastinvn = 1;
-void prepare_invs(int n)
-{
-	invs[0] = invs[1] = 1;
-	if (n <= lastinvn) return;
-	REP(i, lastinvn + 1, n) invs[i] = 1ll * (MOD - invs[MOD % i]) * (MOD / i) % MOD;
-}
-inline poly Der(poly f)
-{
-	int n(deg(f));
-	REP(i, 0, n - 1) f[i] = (i + 1ll) * f[i + 1] % MOD;
-	f.pop_back();
-	return f;
-}
-inline poly Int(const poly &f)
-{
-	int n(deg(f));
-	prepare_invs(n);
-	poly res(n + 2);
-	res[0] = 0;
-	REP(i, 1, n + 1) res[i] = 1ll * f[i-1] * invs[i] % MOD;
-	return res;
-}
-poly ln(const poly &f)
-{
-	int n(deg(f));
-	poly res = Inv(f) * Der(f);
-	res.resize(n + 2);
-	return Int(res);
-}
-poly Sqrt(const poly &f)
-{
-	poly A(1, Less::solve(f[0]));
-	int n(deg(f));
-	for (int d = 2; d / 2 <= n; d <<= 1)
-	{
-		poly H(d, 0);
-		REP(i, 0, min(n, d - 1)) H[i] = f[i];
-		A.resize(d);
-		poly tmp = H * Inv(A);
-		REP(i, 0, d - 1) A[i] = 1ll * Inv2 * (A[i] + tmp[i]) % MOD;
-	}
-	A.resize(n + 1);
-	return A;
-}
-inline poly operator + (poly a, poly b)
-{
-	if (a.size() < b.size()) swap(a, b);
-	REP(i, 0, (int)b.size() - 1) inc(a[i], b[i]);
-	return a;
-}
-inline poly operator - (poly a, poly b)
-{
-	if (a.size() < b.size()) a.resize(b.size());
-	REP(i, 0, (int)a.size() - 1) dec(a[i], b[i]);
-	return a;
-}
-inline poly operator * (int k, poly a)
-{
-	for (auto &i : a) i = 1ll * i * k % MOD;
-	return a;
-}
-inline poly operator * (poly a, int k) {return k * a;}
-inline poly operator / (poly a, int k) {return inv(k) * a;}
-void cdqExp(const poly &f, poly &g, int l, int r)
-{
-	if (l == r)
-	{
-		if (l) g[l] = 1ll * g[l] * invs[l] % MOD;
-		else g[l] = 1;
-		return;
-	}
-	int mid = l + r >> 1;
-	cdqExp(f, g, l, mid);
-	int L = 1;
-	while (L <= r - l + 1) L <<= 1;
-	poly A(L, 0), B(L, 0);
-	REP(i, 0, mid - l) A[i] = g[i + l];
-	REP(i, 0, r - l - 1) B[i] = f[i];
-	NTT(A, L, 1);NTT(B, L, 1);
-	REP(i, 0, L - 1) A[i] = 1ll * A[i] * B[i] % MOD;
-	NTT(A, L, -1);
-	REP(i, mid - l, r - l - 1) inc(g[i + l + 1], A[i]);
-	cdqExp(f, g, mid + 1, r);
-}
-inline poly exp(poly f)
-{
-	int n = deg(f);
-	prepare_invs(n);
-	poly res(n + 1, 0);
-	cdqExp(Der(f), res, 0, n);
-	return res;
 }
 #undef maxn
 
@@ -366,6 +218,8 @@ int mem[maxn];
 i64 Ans;
 
 int dp[maxn], sm[maxn];
+
+unordered_map <int, int> shit[maxn];
 void dfs1(int x)
 {
 	i64 res = 0, s2 = 0, New = 0, s1 = 0;
@@ -380,20 +234,11 @@ void dfs1(int x)
 	(New += s1 * s1) %= MOD;
 	dp[x] = (New * Inv2 + res) % MOD;
 	sm[x] = (mem[x] + s1) % MOD;
-}
-
-unordered_map <int, int> shit[maxn];
-void dfs2(int x)
-{
-	for (int i = bg[x]; i; i = ne[i]) if (to[i] ^ fa[x])
-		dfs2(to[i]);
 	if (x != 1)
 	{
 		int f = fa[x];
 		if (!shit[f].count(siz[x]))
-		{
 			shit[f][siz[x]] = get(ddiv(need[f], siz[x]));
-		}
 		Ans += 1ll * sm[x] * shit[f][siz[x]] % MOD;
 	}
 }
@@ -419,8 +264,6 @@ int main()
 	REP(i, 1, n) mem[i] = get(need[i]);
 	REP(i, 1, n) need[i] = mmul(need[i], n - siz[i]);
 	dfs1(1);
-	Ans = dp[1];
-	dfs2(1);
-	cout << Ans % MOD << endl;
+	cout << (Ans + dp[1]) % MOD << endl;
 	return 0;
 }
