a17f4abc454957bd0361ab98287eff1133e1219a
cnyali-czy <1683053325@qq.com>
2020-06-07 08:42:25 +0800

    MuchFaster

--- gdgzez/noi2020/day5/B/B.cpp
+++ gdgzez/noi2020/day5/B/B.cpp
@@ -16,7 +16,7 @@
 #include <cmath>
 
 using namespace std;
-const int maxn = 1e5 + 10;
+const int maxn = 1e5 + 10, maxN = 5e6;
 
 int bg[maxn], ne[maxn << 1], to[maxn << 1], e;
 inline void add(int x, int y)
@@ -48,18 +48,20 @@ void file(string s)
 #define i64 long long
 int n, q;
 
-int rt, w[maxn];
+int rt, w[maxn], ww[maxn];
 i64 Ans[maxn];
 
+int opt[maxn], l[maxn], r[maxn];
 int blg[maxn], block;
 i64 sum[maxn];
-void getsum(int x, int fa = 0)
+int dfn[maxn], dfs_clock, id[maxn], fa[maxn];
+void dfs0(int x)
 {
-	sum[x] = w[x];
-	for (int i = bg[x]; i; i = ne[i]) if (to[i] ^ fa)
+	dfn[x] = ++dfs_clock;
+	for (int i = bg[x]; i; i = ne[i]) if (to[i] ^ fa[x])
 	{
-		getsum(to[i], x);
-		sum[x] += sum[to[i]];
+		fa[to[i]] = x;
+		dfs0(to[i]);
 	}
 }
 bool need[maxn];
@@ -70,42 +72,35 @@ struct SHIT
 	SHIT(int l = 0, int r = 0, int v = 0, int id = 0) : l(l), r(r), v(v), id(id) {}
 };
 vector <SHIT> Qry[maxn];
-namespace dog
+namespace fenwick
 {
-	int blg[maxn], L[maxn], R[maxn], s[maxn], sp[maxn], ss[maxn], tot;
-	void init()
-	{
-		const int block = sqrt(n);
-		REP(i, 1, n) blg[i] = (i - 1) / block + 1;
-		REP(i, 1, n) R[blg[i]] = i;
-		DEP(i, n, 1) L[blg[i]] = i;
-		tot = blg[n];
-	}
+	int c[maxn];
 	void add(int x, int y = 1)
 	{
-		int b = blg[x];
-		REP(i, b, tot) s[i] += y;
-		REP(i, L[b], x) ss[i] += y;
-		REP(i, x, R[b]) sp[i] += y;
+		while (x <= n)
+		{
+			c[x] += y;
+			x += x & -x;
+		}
 	}
 	int sum(int l, int r)
 	{
-		int bl = blg[l], br = blg[r];
-		if (bl == br) return sp[r] - (l == L[bl] ? 0 : sp[l - 1]);
-		else return ss[l] + sp[r] + s[br - 1] - s[bl];
+		int y = 0;
+		l--;
+		while (r > l) y += c[r], r &= (r - 1);
+		while (l > r) y -= c[l], l &= (l - 1);
+		return y;
 	}
 }
-void dfs(int x, int fa = 0)
+void dfs(int x)
 {
-	dog :: add(x);
+	fenwick :: add(x);
 	for (auto i : Qry[x])
-		Ans[i.id] += 1ll * dog :: sum(i.l, i.r) * i.v;
-	for (int i = bg[x]; i; i = ne[i]) if (to[i] ^ fa)
-		dfs(to[i], x);
-	dog :: add(x, -1);
+		Ans[i.id] += 1ll * fenwick :: sum(i.l, i.r) * i.v;
+	for (int i = bg[x]; i; i = ne[i]) if (to[i] ^ fa[x])
+		dfs(to[i]);
+	fenwick :: add(x, -1);
 }
-int Top;
-pair <int, int> tmp[maxn];
 
 int main()
 {
@@ -113,23 +108,32 @@ int main()
 	file("B");
 #endif
 	n = read();q = read();
-	dog::init();
-	REP(i, 1, n) w[i] = read();
+	REP(i, 1, n) w[i] = ww[i] = read();
 	REP(i, 1, n)
 	{
 		int x(read()), y(read());
 		if (!x) rt = y;
 		else add(x, y), add(y, x);
 	}
-	block = 2 * sqrt(n);
-//	cerr<<block<<endl;
+	dfs0(1);
+	REP(i, 1, n) id[i] = i;
+	sort(id + 1, id + 1 + n, [](int x, int y) {return dfn[x] < dfn[y];});
+	block = max(5., pow(1. * q * n / log2(n + 1), 1. / 3));
+	vector <pair <int, int> > tmp;
 	REP(i, 1, q)
 	{
 		blg[i] = (i - 1) / block + 1;
 		if (blg[i] ^ blg[i - 1])
 		{
-			Top = 0;
-			getsum(rt);
+			tmp.clear();
+			REP(j, 1, n) sum[j] = 0;
+			DEP(j, n, 1)
+			{
+				int k = id[j];
+				sum[k] += w[k];
+				sum[fa[k]] += sum[k];
+			}
+			sum[0] = 0;
 			REP(j, 2, n) sum[j] += sum[j - 1];
 		}
 		int opt = read(), l = read(), r = read();
@@ -137,13 +141,13 @@ int main()
 		{
 			r -= w[l];
 			w[l] += r;
-			tmp[++Top] = make_pair(l, r);
+			tmp.emplace_back(l, r);
 		}
 		else
 		{
 			need[i] = 1;
 			Ans[i] = sum[r] - sum[l - 1];
-			REP(k, 1, Top) Qry[tmp[k].first].emplace_back(l, r, tmp[k].second, i);
+			for (auto k : tmp) Qry[k.first].emplace_back(l, r, k.second, i);
 		}
 	}
 	dfs(rt);
