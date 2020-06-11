b3722548840f9c1943212ea68f6b96b2e9a67a23
cnyali-czy <yms-chenziyang@outlook.com>
2020-06-11 23:39:49 +0800

    shit

--- others/zhengrui/51SX/day1/C/C.cpp
+++ others/zhengrui/51SX/day1/C/C.cpp
@@ -1,57 +1,113 @@
-#include <bits/stdc++.h>
+/*
+	Problem:	C.cpp
+	Time:		2020-06-11 23:26
+	Author:		CraZYali
+	E-Mail:		yms-chenziyang@outlook.com 
+*/
 
-const int N=1<<17;
-std::mt19937 Rand(0721);
-int n,k,Mat[N],Vis[N];
-std::vector<int> v[20],G[N];
-std::vector<std::pair<int,int> > Ans;
+#define REP(i, s, e) for (register int i(s), end_##i(e); i <= end_##i; i++)
+#define DEP(i, s, e) for (register int i(s), end_##i(e); i >= end_##i; i--)
+#define DEBUG fprintf(stderr, "Passing [%s] in Line %d\n", __FUNCTION__, __LINE__)
 
-inline void Out(int x){for(int i=0;i<n;++i)if(x>>i&1)putchar('a'+i);}
+#define chkmax(a, b) (a < (b) ? a = (b) : a)
+#define chkmin(a, b) (a > (b) ? a = (b) : a)
 
-bool DFS(int x)
+#include <set>
+#include <vector>
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+const int maxn = 17, N = 2e5 + 5;
+
+template <typename T>
+inline T read()
 {
-	std::shuffle(G[x].begin(),G[x].end(),Rand),Vis[x]=1;
-	for(int i=0,y,v;i<(int)G[x].size();++i)if(!Vis[Mat[y=G[x][i]]])
+	T ans = 0, flag = 1;
+	char c = getchar();
+	while (!isdigit(c))
 	{
-		if(v=Mat[y],Mat[x]=y,Mat[y]=x,!v||DFS(v))return true;
-		Mat[v]=y,Mat[y]=v,Mat[x]=0;
+		if (c == '-') flag = -1;
+		c = getchar();
 	}
-	return false;
+	while (isdigit(c))
+	{
+		ans = ans * 10 + c - 48;
+		c = getchar();
+	}
+	return ans * flag;
 }
 
-void Link(std::vector<int>& L,int v,int s,int c,int r){
-	if(r>=0&&r+c<=n)c!=n?Link(L,v,s,c+1,r),s>>c&1?Link(L,v|1<<c,s,c+1,r-1):void():L.push_back(v);}
+#define file(FILE_NAME) freopen(FILE_NAME".in", "r", stdin), freopen(FILE_NAME".out", "w", stdout)
 
-void Match(std::vector<int>& u,std::vector<int>& v)
+int n, k, bit[N], match[N];
+vector <int> a[20];
+vector <vector <int> > ans;
+
+void print(int x)
 {
-	std::vector<int> s;
-	s.reserve(u.size()+v.size());
-	for(int i:u)s.push_back(i);
-	for(int i:v)s.push_back(i);
-	for(int i:s)Link(G[i],0,((1<<n)-1)^i,0,k-__builtin_popcount(i));
-	for(int c;;)
-	{
-		std::shuffle(s.begin(),s.end(),Rand),c=0;
-		for(int i:s)Vis[i]=0;
-		for(int i:s)if(!Mat[i])DFS(i);
-		for(int i:u)c+=!Mat[i];
-		if(!c||(&u==&v&&c==int(u.size()&1)))break;
-	}
+	REP(i, 0, n - 1) if (x >> i & 1) putchar('a' + i);
 }
 
 int main()
 {
-	scanf("%d%d",&n,&k);
-	for(int i=1,j;i<1<<n;++i)if((j=__builtin_popcount(i))<=k)v[j].push_back(i);
-	for(int i=1;i<=k-i;++i)Match(v[i],v[k-i]);
-	for(int i=1;i<1<<n;++i)if(__builtin_popcount(i)<=k)
-		{if(Mat[i]>i)Ans.emplace_back(i,Mat[i]);
-		else if(!Mat[i])Ans.emplace_back(i,0);}
-	printf("%d\n",(int)Ans.size());
-	for(auto i:Ans)
+#ifdef CraZYali
+	file("C");
+#endif
+	n = read<int>();k = read<int>();
+	REP(i, 1, (1 << n) - 1)
 	{
-		printf("%d ",1+(i.second>0)),Out(i.first),putchar(i.second?' ':'\n');
-		if(i.second)Out(i.second),putchar('\n');
+		bit[i] = bit[i >> 1] + (i & 1);
+		a[bit[i]].emplace_back(i);
+	}
+	for (int i = 0; i <= k - i; i++)
+	{
+		set <int> S;
+		for (auto j : a[i]) S.emplace(j);
+		int j = 0;
+		while (!S.empty())
+		{
+			if (i == k - i && S.size() == 1) break;
+			int u = *S.rbegin();
+			do
+			{
+				if (j == a[k - i].size()) j = 0;
+				int v = a[k - i][j];
+				if (!(u & v))
+				{
+					if (match[v])
+					{
+						match[match[v]] = 0;
+						S.emplace(match[v]);
+					}
+					else S.erase(v);
+					match[v] = u;
+					match[u] = v;
+					S.erase(u);
+					j++;
+					break;
+				}
+				j++;
+			}while (1);
+		}
+	}
+	REP(i, 1, (1 << n) - 1) if (bit[i] <= k)
+		if (match[i])
+		{
+			if (i < match[i])
+			{
+				vector <int> tmp(1, match[i]);
+				tmp.emplace_back(i);
+				ans.emplace_back(tmp);
+			}
+		}
+		else ans.emplace_back(vector <int> (1, i));
+	cout << ans.size() << endl;
+	for (auto i : ans)
+	{
+		printf("%d", i.size());
+		for (auto j : i) putchar(' '), print(j);
+		puts("");
 	}
 	return 0;
 }
