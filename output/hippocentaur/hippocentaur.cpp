ec7352905f2c21b284ec9709f8564ac232f89446
cnyali-czy <yms-chenziyang@outlook.com>
2020-09-30 09:50:05 +0800

    good

---
+++ output/hippocentaur/hippocentaur.cpp
@@ -0,0 +1,75 @@
+/*
+	Problem:	hippocentaur.cpp
+	Time:		2020-09-30 09:27
+	Author:		CraZYali
+	E-Mail:		yms-chenziyang@outlook.com 
+*/
+
+#define REP(i, s, e) for (register int i(s), end_##i(e); i <= end_##i; i++)
+#define DEP(i, s, e) for (register int i(s), end_##i(e); i >= end_##i; i--)
+#define DEBUG fprintf(stderr, "Passing [%s] in Line %d\n", __FUNCTION__, __LINE__)
+
+#define chkmax(a, b) (a < (b) ? a = (b) : a)
+#define chkmin(a, b) (a > (b) ? a = (b) : a)
+
+#include <iostream>
+#include <cstdio>
+#define i64 long long
+
+using namespace std;
+const int maxn = 2e7 + 10, MOD = 998244353;
+
+i64 power_pow(i64 base, int b)
+{
+	i64 ans = 1;
+	while (b)
+	{
+		if (b & 1) (ans *= base) %= MOD;
+		(base *= base) %= MOD;
+		b >>= 1;
+	}
+	return ans;
+}
+#define inv(x) power_pow(x, MOD - 2)
+
+	template <typename T>
+inline T read()
+{
+	T ans = 0, flag = 1;
+	char c = getchar();
+	while (!isdigit(c))
+	{
+		if (c == '-') flag = -1;
+		c = getchar();
+	}
+	while (isdigit(c))
+	{
+		ans = ans * 10 + c - 48;
+		c = getchar();
+	}
+	return ans * flag;
+}
+
+#define file(FILE_NAME) freopen(FILE_NAME".in", "r", stdin), freopen(FILE_NAME".out", "w", stdout)
+
+int n, fac[maxn << 1], Inv[maxn << 1];
+inline i64 C(int n, int m) {return n < m || m < 0 ? 0 : 1ll * fac[n] * Inv[m] % MOD * Inv[n - m] % MOD;}
+
+void init(int n)
+{
+	fac[0] = 1;
+	REP(i, 1, n) fac[i] = 1ll * fac[i - 1] * i % MOD;
+	Inv[n] = inv(fac[n]);
+	DEP(i, n - 1, 0) Inv[i] = (i + 1ll) * Inv[i + 1] % MOD;
+}
+
+int main()
+{
+	file("hippocentaur12");
+	n = read<int>();
+	init(n << 1);
+	i64 ans = 8 * C(n + n, n) - 3ll * n % MOD * n - 2 * n - 7;
+	ans = (ans % MOD + MOD) % MOD;
+	cout << ans << endl;
+	return 0;
+}
