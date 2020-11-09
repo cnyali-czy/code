1eec3c494b725b32a870cd2e9cf2f064a71ae492
cnyali-czy <yms-chenziyang@outlook.com>
2020-11-09 16:12:41 +0800

    shit

--- luogu/std.cpp
+++ luogu/std.cpp
@@ -1,71 +1,103 @@
+#define  REP(i, s, e) for (register int i = s; i <= e; i++)
+#define DREP(i, s, e) for (register int i = s; i >= e; i--)
+#define DEBUG fprintf(stderr, "Passing [%s] in Line %d\n", __FUNCTION__, __LINE__)
 
-#include<bits/stdc++.h>
-#define maxn 100
+#define chkmax(a, b) a = max(a, b)
+#define chkmin(a, b) a = min(a, b)
+
+#include <bits/stdc++.h>
+
 using namespace std;
-template<typename T> inline void read(T &x)
+const int maxn = 1e5 + 10;
+
+template <typename T> T read()
 {
-    x=0;char c=getchar();bool flag=false;
-    while(!isdigit(c)){if(c=='-')flag=true;c=getchar();}
-    while(isdigit(c)){x=(x<<1)+(x<<3)+(c^48);c=getchar();}
-    if(flag)x=-x;
+	T ans = 0, p = 1;
+	char c = getchar();
+	while (!isdigit(c))
+	{
+		if (c == '-') p = -1;
+		c = getchar();
+	}
+	while (isdigit(c))
+	{
+		ans = ans * 10 + c - 48;
+		c = getchar();
+	}
+	return ans * p;
 }
-int T,n,ans,tot,flag;
-int p[maxn],sg[maxn];
-bool vis[maxn];
-void SG()
+
+struct node
 {
-    for(int i=n-1;i;--i)
-    {
-        memset(vis,false,sizeof(vis));
-        for(int j=i+1;j<=n;++j)
-            for(int k=j;k<=n;++k)
-                vis[sg[j]^sg[k]]=true;
-        int t=0;
-        while(1)
-        {
-            if(!vis[t])
-            {
-                sg[i]=t;
-                break;
-            }
-            t++;
-        }
-    }
+	int val, id, dis;
+	node *l, *r;
+	node() {l = r = NULL;dis = 0;}
+	node(int _val, int _id, int _dis = 0) : dis(_dis), id(_id), val(_val) {l = r = NULL;}
+};
+int dis(node *A) {return A == NULL ? -1 : A -> dis;}
+
+struct Heap
+{
+	node *root;
+	Heap() {root = NULL;}
+	void init(int val, int id) {root = new node(val, id);}
+	node* merge(node *A, node *B)
+	{
+		if (A == NULL) return B;
+		else if (B == NULL) return A;
+		else
+		{
+			if (A -> val > B -> val || A -> val == B -> val && A -> id > B -> id) swap(A, B);
+			A -> r = merge(A -> r, B);
+			if (dis(A -> l) < dis(A -> r)) swap(A -> l, A -> r);
+			A -> dis = dis(A -> r) + 1;
+			return A;
+		}
+	}
+	pair <int, int> top() {return make_pair(root -> val, root -> id);}
+	void pop() {root = merge(root -> l, root -> r);}
+	Heap operator + (Heap B) {Heap res = *this;res.root = merge(res.root, B.root);return res;}
+	Heap operator += (Heap B) {return *this = *this + B;}
+}H[maxn];
+int fa[maxn];
+int find(int x) {return fa[x] == x ? fa[x] : fa[x] = find(fa[x]);}
+
+bool del[maxn];
+void uni(int x, int y)
+{
+	if (del[x] || del[y]) return;
+	x = find(x);y = find(y);
+	if (x ^ y)
+	{
+		fa[x] = y;
+		H[y] += H[x];
+	}
 }
+int n, m;
+
 int main()
 {
-    read(T);
-    while(T--)
-    {
-        read(n),sg[n]=ans=tot=flag=0;
-        for(int i=1;i<=n;++i) read(p[i]);
-        SG();
-		for(int i=1;i<=n;i++)printf("%d%c",sg[i],i==n?'\n':' ');
-        for(int i=1;i<=n;++i)
-            if(p[i]%2)
-                ans^=sg[i];
-        for(int i=1;i<=n;++i)
-        {
-            if(!p[i]) continue;
-            for(int j=i+1;j<=n;++j)
-            {
-                for(int k=j;k<=n;++k)
-                {
-                    if((ans^sg[i]^sg[j]^sg[k])==0)
-                    {
-						cout<<i<<' '<<j<<' '<<k<<endl;
-                        tot++;
-                        if(!flag)
-                        {
-                            flag=true;
-                            printf("%d %d %d\n",i-1,j-1,k-1);
-                        }
-                    }
-                }
-            }
-        }
-        if(!flag) puts("-1 -1 -1");
-        printf("%d\n",tot);
-    }
-    return 0;
+#ifdef CraZYali
+	freopen("3377-new-new-new.in", "r", stdin);
+	freopen("std.out", "w", stdout);
+#endif
+	cin >> n >> m;
+	REP(i, 1, n) H[i].init(read<int>(), fa[i] = i);
+	while (m --> 0)
+	{
+		register int opt = read<int>(), x = read<int>();
+		if (opt == 1 ) 
+		{
+			register int y = read<int>();
+			uni(x, y);
+		}
+		else if (!del[x])
+		{
+			printf("%d %d\n", H[find(x)].top().first, H[find(x)].top().second);
+			del[H[find(x)].top().second] = 1;
+			H[find(x)].pop();
+		}
+		else printf("-1\n");
+	}
+	return 0;
 }
---
+++ others/zhengrui/20noip/day16/A/A.cpp
@@ -0,0 +1,49 @@
+#define REP(i, s, e) for (register int i(s), end_##i(e); i <= end_##i; i++)
+#define DEP(i, s, e) for (register int i(s), end_##i(e); i >= end_##i; i--)
+#define DEBUG fprintf(stderr, "Passing [%s] in Line %d\n", __FUNCTION__, __LINE__)
+
+#define chkmax(a, b) (a < (b) ? a = (b) : a)
+#define chkmin(a, b) (a > (b) ? a = (b) : a)
+
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+const int maxn = 1e6 + 10;
+
+	template <typename T>
+inline T read()
+{
+	T ans = 0, flag = 1;
+	char c = getchar();
+	while (!isdigit(c))
+	{
+		if (c == '-') flag = -1;
+		c = getchar();
+	}
+	while (isdigit(c))
+	{
+		ans = ans * 10 + (c - 48);
+		c = getchar();
+	}
+	return ans * flag;
+}
+
+#define file(FILE_NAME) freopen(FILE_NAME".in", "r", stdin), freopen(FILE_NAME".out", "w", stdout)
+
+int main()
+{
+#ifdef CraZYali
+	file("A");
+#endif
+	int Max = 0, Min = 0;
+	int n = read<int>(), m = read<int>();
+	while (m--)
+	{
+		int x = read<int>();
+		Max += max(0, x - 2);
+		if (x > 2) Min += x / 2;
+	}
+	cout << Max << ' ' << Min << endl;
+	return 0;
+}
