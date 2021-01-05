c7192327a6a74668e86d9a3a81fdc68e3571f230
cnyali-czy <yms-chenziyang@outlook.com>
2021-01-05 14:46:13 +0800

    good

---
+++ 1227/G.cpp
@@ -0,0 +1,39 @@
+#define REP(i, s, e) for (register int i(s), end_##i(e); i <= end_##i; i++)
+
+#include <algorithm>
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+const int maxn = 1000 + 10;
+
+int n, a[maxn], c[maxn][maxn];
+
+int main()
+{
+#ifdef CraZYali
+	freopen("G.in", "r", stdin);
+	freopen("G.out", "w", stdout);
+#endif
+	scanf("%d", &n);
+	REP(i, 1, n) scanf("%d", a + i);
+	static int id[maxn];
+	REP(i, 1, n) id[i] = i;
+	sort(id + 1, id + 1 + n, [&](int x, int y) {return a[x] < a[y];});
+	REP(i, 1, n)
+	{
+		int x = a[id[i]];
+		for (int j = i; x--;)
+		{
+			c[j][id[i]] = 1;
+			j++;if (j > n + 1) j = 1;
+		}
+	}
+	REP(i, 1, n + 1)
+	{
+		REP(j, 1, n) putchar(c[i][j] + 48);
+		puts("");
+	}
+	return 0;
+}
